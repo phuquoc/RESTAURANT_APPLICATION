ea1b721fdb2ad8e50fbcad92c3a9838cc3859038
phan nho anh <prydz.pa@gmail.com>
2016-11-29 14:05:34 +0700

    food_items + contact_us

--- restaurant/db/schema.rb
+++ restaurant/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161129065924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "food_items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "section"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
