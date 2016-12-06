3fc02846b40fc1dc84ff430644d8d46b8ba5f351
phuquoc <votruongtoan123@gmail.com>
2016-12-06 21:26:32 +0700

    save image to database

--- restaurant/app/models/food_item.rb
+++ restaurant/app/models/food_item.rb
@@ -1,12 +1,17 @@
 class FoodItem < ApplicationRecord
   validates :name, :section, :price, presence: true
 #  belongs_to :section
+
+mount_uploader :image, AttachmentUploader # Tells rails to use this uploader for this model.
+
+
   def image_url
     if image.present?
       image
     else
-      "http://loremflickr.com/320/240/#{CGI.escape name}"
+      #"http://loremflickr.com/320/240/#{CGI.escape name}"
     # onchange="readURL(this)
+    "#"
     end
   end
 
