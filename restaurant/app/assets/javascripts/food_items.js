0bf1a7050ba2156d703c6cbf0343a5f6dfaa5bbc
phuquoc <votruongtoan123@gmail.com>
2016-12-12 18:37:36 +0700

    unsuccessful javascript and jquery

--- restaurant/app/assets/javascripts/food_items.js
+++ restaurant/app/assets/javascripts/food_items.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function onMouseOver(row){
+  //debugger
+ row.style.backgroundColor = "blue"
+  row.className = 'hover-row';
+}
+
+function outMouseOut(row){
+  row.className = "";
+}
+
+$(document).ready(function(){
+  $("table > tbody > tr").hover(function(){
+    $(this).addClass('hover-row')
+  }, function(){
+      $(this).removeClass('hover-row')
+  })
+});
